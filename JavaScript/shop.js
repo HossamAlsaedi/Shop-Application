165fefd83ddb67753168a473bc122ee92ef20f11
HussamAlsaedi <hossam_ksa@hotmail.com>
2024-10-09 14:31:18 +0300

    changing the alert context

--- JavaScript/shop.js
+++ JavaScript/shop.js
@@ -2782,7 +2782,7 @@ function addNewProduct(productId, img) {
     showAlert("success", "Product has been added");
 
     setTimeout(() => {
-        showAlert("", "Please note: To keep your changes on products, creating an account is required.", 3000);
+        showAlert("", "Note:Create an account to keep your changes on products", 3000);
     }, 2000);
 }
 
